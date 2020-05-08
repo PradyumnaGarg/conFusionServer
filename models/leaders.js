b9d2df3ada7fbfc23142026d16f5926a1e0dd8bc
Veteran <pradumnagarg@hotmail.com>
2020-05-09 02:45:45 +0530

    Assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const leadersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+})
+
+var leaders = mongoose.model('Leaders', leadersSchema);
+
+module.exports = leaders;
