9f00a73ad856f71fcf18f44e13cf0c7ec7dc3cc5
Veteran <pradumnagarg@hotmail.com>
2020-05-09 16:50:05 +0530

    Basic Authentication

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const mongoose = require("mongoose");
 const Leaders = require("../models/leaders");
 
 var leaderRouter = express.Router();
